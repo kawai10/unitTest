ba2b6be75e1b7e419ff7a7f7d01922adc3219e78
kawai10 <77530278+kawai10@users.noreply.github.com>
2022-04-07 22:35:25 +0900

    계산기 테스트 코드 만들어보기

---
+++ src/test/calculator.test.js
@@ -0,0 +1,42 @@
+const Calculator = require("../calculator.js");
+//각각의 테스트는 독립적으로 서로에게 영향을 주지 않도록 실행되어야 함.
+describe("Calculator", () => {
+  let cal;
+  beforeEach(() => {
+    cal = new Calculator();
+  });
+
+  it("inits with 0", () => {
+    expect(cal.value).toBe(0);
+  });
+
+  it("sets", () => {
+    cal.set(9);
+    expect(cal.value).toBe(9);
+  });
+
+  it("clear", () => {
+    cal.set(9);
+    cal.clear();
+    expect(cal.value).toBe(0);
+  });
+
+  it("add", () => {
+    cal.set(1);
+    cal.add(2);
+    expect(cal.value).toBe(3);
+  });
+
+  describe("divides", () => {
+    it("0 / 0 === NaN", () => {
+      cal.divide(0);
+      expect(cal.value).toBe(NaN);
+    });
+
+    it("1 / 0 === Infinity", () => {
+      cal.set(1);
+      cal.divide(0);
+      expect(cal.value).toBe(Infinity);
+    });
+  });
+});
